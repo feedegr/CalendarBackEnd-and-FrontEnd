5918436301cef410d5c2fe59c646691e4cebe804
feede.gr <feedecs@hotmail.com>
2022-07-28 12:52:41 -0300

    anor

--- database/config.js
+++ database/config.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.DB_CNN, {
-            //useNewUrlParser: true,
-           // useUnifiedTopology: true,
-           // useCreateIndex: true
+            
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
         });
 
         console.log('DB is connected');
